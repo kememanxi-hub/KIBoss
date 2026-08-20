430f4c1065d8d7da297d12caa188c545432ffcc1
kememanxi-hub <kememanxi@gmail.com>
2026-08-20 09:03:24 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,18 +4,24 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Server KIBOBoss Berjalan dengan Baik!');
+  res.send('Server KIBOBoss Aktif!');
+});
+
+// Endpoint untuk melayani file json langsung lewat kode
+app.get('/users/kibo.json', (req, res) => {
+  res.json({
+    username: "kibo",
+    password: "kibboss",
+    status: "active"
+  });
 });
 
 app.post('/api/login', (req, res) => {
   const { username, password } = req.body;
-  
-  // Mengubah data login sesuai permintaan Anda
   if (username === 'kibo' && password === 'kibboss') {
     return res.json({ status: true, message: 'Login Berhasil!' });
   }
-  
-  res.status(401).json({ status: false, message: 'Username atau Password salah!' });
+  res.status(401).json({ status: false, message: 'Gagal Login' });
 });
 
 module.exports = app;
